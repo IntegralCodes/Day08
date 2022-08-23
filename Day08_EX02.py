d3bb5eb38d07e95a34b8da8a59add1318db647f9
bigal <bigalex006@naver.com>
2022-08-23 16:23:59 +0900

    Day 08

--- Day08_EX02.py
+++ Day08_EX02.py
@@ -3,10 +3,15 @@ class Member: #나는 개똥벌래
         self.name = n
         self.count = m
     def enter(self):
-        print("회원{}님 이 입장하였습니다.".format(self.name))
+        print("회원 {}님 이 입장하였습니다.".format(self.name))
         self.count = self.count + 1
         print("{}님 입장 횟수는 {}회 입니다.".format(self.name, self.count))
 
 jong = Member("종민", 0)
 jong.enter()
 jong.enter()
+jong.enter()
+
+tom = Member("톰", 0)
+tom.enter()
+tom.enter()
---
+++ Day08_EX03.py
@@ -0,0 +1,16 @@
+class Family:
+    def __init__(self, n, a):
+        self.name = n
+        self.address = a
+
+    def info(self):
+        print("저는 {} 이구요, {}에 살고 있어요.".format(self.name, self.address))
+
+    def move(self, a):
+        self.address = a
+
+father = Family("아빠", "서울")
+father.move("부산")
+father.info()
+son = Family("아들", "서울")
+son.info()
